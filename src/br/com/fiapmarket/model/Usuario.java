fc7802f58bd3d74ad77858b6bfa960e114ed29c6
Israel-Nunes <israelkcnunes@gmail.com>
2026-05-11 20:07:12 -0300

    feat: aula2 - adiciona metodos com parametros e retorno

--- src/br/com/fiapmarket/model/Usuario.java
+++ src/br/com/fiapmarket/model/Usuario.java
@@ -1,9 +1,41 @@
 package br.com.fiapmarket.model;
- 
+
 public class Usuario {
- 
+
     public String nome;
     public int idade;
     public String email;
- 
+
+    // MÉTODO: atualizar email com validação
+    public void atualizarEmail(String novoEmail) {
+
+        if (novoEmail == null || !novoEmail.contains("@")) {
+            System.out.println("Erro: email inválido.");
+            return;
+        }
+
+        email = novoEmail;
+        System.out.println("Email atualizado com sucesso!");
+    }
+
+    // MÉTODO: atualizar idade com validação
+    public void atualizarIdade(int novaIdade) {
+
+        if (novaIdade <= 0) {
+            System.out.println("Erro: idade inválida.");
+            return;
+        }
+
+        idade = novaIdade;
+        System.out.println("Idade atualizada com sucesso!");
+    }
+
+    // MÉTODO: exibir dados
+    public void exibirDados() {
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Email: " + email);
+        System.out.println("----------------------");
+    }
 }
